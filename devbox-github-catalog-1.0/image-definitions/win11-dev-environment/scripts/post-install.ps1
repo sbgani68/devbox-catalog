a6cb8ffbe64f338b728e7f6d1e278e9ed24f7aa8
Seyed Ahamed <seyed.ahamed@sainsburys.co.uk>
2025-12-11 20:59:18 +0000

    Delete devbox-github-catalog-1.0 directory

--- devbox-github-catalog-1.0/image-definitions/win11-dev-environment/scripts/post-install.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Running post-install configuration..."
-$dockerPath = "C:\Program Files\Docker\Docker"
-if (Test-Path $dockerPath) {
-    setx PATH "$env:PATH;$dockerPath"
-}
-Write-Host "Post-install configuration complete."
